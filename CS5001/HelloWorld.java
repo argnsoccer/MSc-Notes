ec9f3d4d2e08c93c76138e5466a7d1fcbfe94e2a
argnsoccer <amgras@smu.edu>
2018-09-21 14:06:50 +0100

    more stuff

---
+++ CS5001/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        if(args.length == 0)
+        {
+            System.out.println("Hello World");
+        }
+        else
+        {
+            System.out.println("Hello " + args[0]);
+        }
+        
+    }
+}
